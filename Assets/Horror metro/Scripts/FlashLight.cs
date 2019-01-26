42cefbede67220ab6bb739dfa17a65ff8de6d385
DESKTOP-FVSCF23\htw75 <htw750128@gmail.com>
2019-01-26 22:46:03 +0800

    第一次上傳

---
+++ Assets/Horror metro/Scripts/FlashLight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class FlashLight : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            gameObject.GetComponent<Light>().enabled = !gameObject.GetComponent<Light>().enabled;
+        }
+
+	}
+}
